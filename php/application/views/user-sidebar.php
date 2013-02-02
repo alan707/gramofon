71b8af0519c92bd6156c93b3bf59958424b72652
Maurice W <maurice.williams@ge.com>
2013-02-02 16:02:13 -0500

    renaming the CSS classes to be more generic

--- php/application/views/user-sidebar.php
+++ php/application/views/user-sidebar.php
@@ -1,21 +1,21 @@
-<ul class="user-sidebar">
-    <li class="user-sidebar-item">
-        <span class="user-sidebar-name">Sounds</span>
-        <span class="user-sidebar-value"><?= sizeof($user->audio_clips) ?></span>
+<ul class="sidebar">
+    <li class="sidebar-item">
+        <span class="sidebar-name">Sounds</span>
+        <span class="sidebar-value"><?= sizeof($user->audio_clips) ?></span>
     </li>
     
-    <li class="user-sidebar-item">
-        <span class="user-sidebar-name">Plays</span>
-        <span class="user-sidebar-value">42</span>
+    <li class="sidebar-item">
+        <span class="sidebar-name">Plays</span>
+        <span class="sidebar-value">42</span>
     </li>
     
-    <li class="user-sidebar-item">
-        <span class="user-sidebar-name">Followers</span>
-        <span class="user-sidebar-value">77</span>
+    <li class="sidebar-item">
+        <span class="sidebar-name">Followers</span>
+        <span class="sidebar-value">77</span>
     </li>
     
-    <li class="user-sidebar-item">
-        <span class="user-sidebar-name">Following</span>
-        <span class="user-sidebar-value">21</span>
+    <li class="sidebar-item">
+        <span class="sidebar-name">Following</span>
+        <span class="sidebar-value">21</span>
     </li>
 </ul>
