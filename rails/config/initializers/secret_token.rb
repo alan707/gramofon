9fd4d245d63d35c273a982116ca0c7a4ab327b68
Maurice W <maurice.williams@ge.com>
2013-02-02 11:08:40 -0500

    cleaning up rails folder

--- rails/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Gramofon::Application.config.secret_token = '6d9022309f50d856bdb2567a61d2f2cb972dc3565a379a3cb6c4098745476e96158c79e77459b0c155ed0c0b15d815158e6216d78a177a32225b81a6bd28e770'
