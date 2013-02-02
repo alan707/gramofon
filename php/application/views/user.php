80357ca60a166f8ee73db48586e045c8abc3167c
Maurice W <maurice.williams@ge.com>
2013-02-02 16:29:11 -0500

    good bye profile sidebar

--- php/application/views/user.php
+++ php/application/views/user.php
@@ -6,15 +6,13 @@
                 <img class="user-block-image" src="http://www.androidtablets.net/forum/attachments/sylvania-tablets/4266d1318424401-what-mini-tablet-express-plus-nailed_it_re_20th_century_fox_theme_on_flute-s240x320-227191.jpg" />
             </div>
 
-            <div class="user-block-details-wrapper eight columns">
+            <div class="user-block-details-wrapper ten columns">
                 <h2 class="user-block-name"><?= $user->firstname ?> <?= $user->lastname ?></h2>
-                <a href="" class="user-block-username"><?= $user->username ?></a>
+                <a href="" class="user-block-username"><?= $user->username ?></a><a class="tiny button user-follow-button" href="#">Follow</a>
                 <div><?= sizeof($user->audio_clips) ?> sounds</div>
-                <div><a class="tiny button" href="#">Follow</a></div>
-            </div>
-            
-            <div class="two columns">
-                <? $this->load->view('user-sidebar.php') ?>
+                <div>42 Plays</div>
+                <div>77 Followers</div>
+                <div>21 Following</div>
             </div>
         </div>
 
