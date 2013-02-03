e6f98944dda101f0c8b3cdff8095d3dd258324c1
Maurice W <maurice.williams@ge.com>
2013-02-02 21:57:16 -0500

    better facebook implemetnation
    now with super awesome work-aroud for CI

--- php/application/controllers/user.php
+++ php/application/controllers/user.php
@@ -10,6 +10,20 @@ class User extends CI_Controller {
         $CI->config->load("facebook",TRUE);
         $config = $CI->config->item('facebook');
         $this->load->library('facebook', $config);
+        
+    // Stop cache
+    $CI->output->set_header("Cache-Control: private, no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
+    $CI->output->set_header("Pragma: no-cache");
+
+
+    // Repopulate _REQUEST ... Facebook needs it.
+    $request_uri = $_SERVER['REQUEST_URI'];
+    $request_uri = explode('?',$request_uri);
+    if(count($request_uri) > 1) {
+        parse_str($request_uri[1], $_REQUEST);
+    }
+        
+        
     }
 
     public function index( $username )
@@ -38,11 +52,14 @@ class User extends CI_Controller {
     public function facebook()
     {
         $user = $this->facebook->getUser();
-        print_r($this->facebook);
+        
+        echo "User ID: " . $user;
+        
         if($user) {
             try {
                 $user_info = $this->facebook->api('/me');
                 echo '<pre>'.htmlspecialchars(print_r($user_info, true)).'</pre>';
+                echo "<br/><br/><a href=\"{$this->facebook->getLogoutUrl()}\">Logout of Facebook!</a>";
             } catch(FacebookApiException $e) {
                 echo '<pre>'.htmlspecialchars(print_r($e, true)).'</pre>';
                 $user = null;
@@ -50,6 +67,8 @@ class User extends CI_Controller {
         } else {
             echo "<a href=\"{$this->facebook->getLoginUrl()}\">Login using Facebook!</a>";
         }
+        
+        
     }
 
 }
