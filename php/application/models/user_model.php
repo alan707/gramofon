f296006c10164911be62ed9afa86826e1ea3ee63
Maurice W <maurice.williams@ge.com>
2013-02-03 10:59:54 -0500

    Facebook pictures on profile pages
    Individual profile pages will now display your profile picture from
    Facebook (assuming your ID is already in the DB)

--- php/application/models/user_model.php
+++ php/application/models/user_model.php
@@ -8,8 +8,19 @@ class User_model extends CI_Model {
         
         $json = file_get_contents("http://gramofon.herokuapp.com/users/$username.json");
         
+        //grab the facebookID from the users Gramofon profile
+        $userFacebookId = json_decode($json);
+        $userFacebookId = $userFacebookId->facebook_id;
+        
+        //use Fb's OpenGraph API to grab their user data (right now only profile picture)
+        $facebookJson = file_get_contents("http://graph.facebook.com/" . $userFacebookId . "?fields=name,picture.height(200)");
+        
+        $facebookData = json_decode($facebookJson);
+        
+        
         if ( !empty($json) ) {
             $user = json_decode($json);
+            $user->profile_picture = $facebookData->picture->data->url;
         }
         
         return $user;
