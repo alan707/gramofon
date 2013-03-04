e7e79583dfdba878702c00b34fb93fa22b6bb5f3
Dan Trenz <dtrenz@gmail.com>
2013-03-04 08:41:51 -0500

    patched web app to work with API user/audio_clips lookupby user_id, instead of username.

--- php/application/models/user_model.php
+++ php/application/models/user_model.php
@@ -10,23 +10,6 @@ class User_model extends CI_Model {
     public function get_user_profile( $username )
     {                
         return $this->gramofonclient->get_user_by_username( $username );
-
-//        //grab the facebookID from the users Gramofon profile
-//        $userFacebookId = json_decode($json);
-//        $userFacebookId = $userFacebookId->facebook_id;
-//        
-//        //use Fb's OpenGraph API to grab their user data (right now only profile picture)
-//        $facebookJson = file_get_contents("http://graph.facebook.com/" . $userFacebookId . "?fields=name,picture.height(200)");
-//        
-//        $facebookData = json_decode($facebookJson);
-        
-      
-//        if ( !empty($json) ) {
-//            $user = json_decode($json);
-//            $user->profile_picture = $facebookData->picture->data->url;
-//        }
-        
-//        return $user;
     }
     
     public function get_facebook_profile( $facebook_id )
