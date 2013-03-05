0ab5c260afacb09b65455eacf5d2a0907ddefe55
Alan Mond <alan.mond@gmail.com>
2013-03-04 22:05:54 -0500

    changed user controller

--- rails/gramofon_rails/app/controllers/users_controller.rb
+++ rails/gramofon_rails/app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
  end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find_by_username(params[:id])
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
