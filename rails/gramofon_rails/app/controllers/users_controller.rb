ba7a849cf7295c56b1f8683ec7ee718726e7e2f3
Alan Mond <alan.mond@gmail.com>
2013-03-04 22:35:25 -0500

    repositioned clear button so that it is covering the entire ShareSoundView

--- rails/gramofon_rails/app/controllers/users_controller.rb
+++ rails/gramofon_rails/app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-    @user = User.find(params[:id])
+    @user = User.find_by_username(params[:id])
   end
 
   # POST /users
@@ -73,7 +73,7 @@ class UsersController < ApplicationController
   # PUT /users/1
   # PUT /users/1.json
   def update
- @user = User.find(params[:id])
+ @user = User.find_by_username(params[:id])
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
@@ -89,7 +89,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
- @user = User.find(params[:id])
+ @user = User.find_by_username(params[:id])
     @user.destroy
 
     respond_to do |format|
